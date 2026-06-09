0c3e114e77afd9532c2f4c59af19fb0cb5acab39
Bear Evans <bear-evans@outlook.com>
2026-06-08 22:39:30 -0400

    Added Settings loader framework and empty block behavior

---
+++ Bear's Shifty Earth/src/BlockBehaviorShiftyFalling.cs
@@ -0,0 +1,25 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace BearsShiftyEarth
+{
+    /// <summary>
+    /// Custom block behavior for Shifty Earth logic. Inherits from BlockBehaviorUnstableFalling and acts as a wrapper for the vanilla falling logic. Does not replace or overwrite UnstableFalling's logic and so should be safe to patch from other mods.
+    /// </summary>
+    public class BlockBehaviorShiftyFalling : BlockBehaviorUnstableFalling
+    {
+        #region Fields
+
+        public bool isShifty = false;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public BlockBehaviorShiftyFalling(Block block) : base(block)
+        {
+        }
+
+        #endregion Constructors
+    }
+}
