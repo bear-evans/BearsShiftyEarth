c8086a6e7114827c92d246fe1b8dd0a8b4310caf
Bear Evans <bear-evans@outlook.com>
2026-06-08 23:33:18 -0400

    Unstable behavior successfully hijacked

--- Bear's Shifty Earth/BearsShiftyEarthModSystem.cs
+++ Bear's Shifty Earth/BearsShiftyEarthModSystem.cs
@@ -3,27 +3,64 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-namespace Bear_s_Shifty_Earth
+namespace BearsShiftyEarth
 {
+    /// <summary>
+    /// Core mod system for Bear's Shifty Earth. Handles registration of ShiftyFalling block behavior.
+    /// </summary>
     public class BearsShiftyEarthModSystem : ModSystem
     {
+        #region Properties
+
+        public static ICoreAPI? Api { get => API; }
+        public static ILogger? Logger { get => LOGGER; }
+
+        #endregion Properties
+
+        #region Fields
+
+        private static ICoreAPI? API;
+        private static ILogger? LOGGER;
+
+        #endregion Fields
+
         #region Methods
+
+        public static void EmitChatMessage(string message)
+        {
+            LOGGER?.Chat(message);
+        }
+
+        public static void EmitNotificationMessage(string message)
+        {
+            LOGGER?.Notification(message);
+        }
+
+        public static void EmitErrorMessage(string message)
+        {
+            LOGGER?.Error(message);
+        }
 
         // Called on server and client
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            // save references for use by the block behavior
+            API = api;
+            LOGGER = Mod.Logger;
+
+            // We register our custom behavior under the default behavior's name to route the logic through us
+            api.RegisterBlockBehaviorClass("UnstableFalling", typeof(BlockBehaviorShiftyFalling));
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("bearsshiftyearth:hello"));
+            Mod.Logger.Notification(Lang.Get("bearsshiftyearth:hello"));
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("bearsshiftyearth:hello"));
+            Mod.Logger.Notification(Lang.Get("bearsshiftyearth:hello"));
         }
 
         #endregion Methods
--- Bear's Shifty Earth/src/BlockBehaviorShiftyFalling.cs
+++ Bear's Shifty Earth/src/BlockBehaviorShiftyFalling.cs
@@ -1,4 +1,5 @@
 ﻿using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
 namespace BearsShiftyEarth
@@ -21,5 +22,47 @@ namespace BearsShiftyEarth
         }
 
         #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// OnBlockPlaced by default just triggers TryFalling, so we override it to inject our own code.
+        /// </summary>
+        public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ref EnumHandling handling)
+        {
+            // skip all custom logic if we are not a shifty block.
+            if (!isShifty) {
+                base.OnBlockPlaced(world, blockPos, ref handling);
+            }
+
+            // TODO: obviously we want our own custom code in here eventually
+
+            base.OnBlockPlaced(world, blockPos, ref handling);
+        }
+
+        /// <summary>
+        /// OnNeighborBlockChange by default just tries to trigger a fall, so we override it to inject our own support code.
+        /// </summary>
+        public override void OnNeighbourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos, ref EnumHandling handling)
+        {
+            // default to vanilla if we are not a shifty block
+            if (!isShifty) {
+                base.OnNeighbourBlockChange(world, pos, neibpos, ref handling);
+            }
+
+            // TODO: Custom code redirects go here
+
+            base.OnNeighbourBlockChange(world, pos, neibpos, ref handling);
+        }
+
+        /// <summary>
+        /// Checks to see if the block is configured to be a shifty block and should have our custom logic applied. Returns true if it should be targeted by custom logic, false if it behaves as vanilla.
+        /// </summary>
+        public bool IsThisBlockKindaShifty(Block block)
+        {
+            return false;
+        }
+
+        #endregion Methods
     }
 }
